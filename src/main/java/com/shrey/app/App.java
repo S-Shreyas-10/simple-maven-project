29fb25d3f74d41e9deb730a41a84fb4ee89b82ca
S Shreyas <shrey.port@ibm.com>
2026-02-09 15:53:31 +0530

    First Commit

---
+++ src/main/java/com/shrey/app/App.java
@@ -0,0 +1,8 @@
+package com.shrey.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Simple Maven App 🚀");
+        System.out.println("Build and run successful.");
+    }
+}
---
+++ src/test/java/com/shrey/app/AppTest.java
@@ -0,0 +1,12 @@
+package com.shrey.app;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldPassBasicTest() {
+        assertTrue(true);
+    }
+}
